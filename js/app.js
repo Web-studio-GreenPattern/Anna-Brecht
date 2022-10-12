8985bc95e8a4886619dfc9c3d9f4c3f900508d67
Web-studio GreenPattern <110899018+Web-studio-GreenPattern@users.noreply.github.com>
2022-10-12 16:40:33 +0700

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,65 @@
+(() => {
+    "use strict";
+    function isWebp() {
+        function testWebP(callback) {
+            let webP = new Image;
+            webP.onload = webP.onerror = function() {
+                callback(2 == webP.height);
+            };
+            webP.src = "data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA";
+        }
+        testWebP((function(support) {
+            let className = true === support ? "webp" : "no-webp";
+            document.documentElement.classList.add(className);
+        }));
+    }
+    let isMobile = {
+        Android: function() {
+            return navigator.userAgent.match(/Android/i);
+        },
+        BlackBerry: function() {
+            return navigator.userAgent.match(/BlackBerry/i);
+        },
+        iOS: function() {
+            return navigator.userAgent.match(/iPhone|iPad|iPod/i);
+        },
+        Opera: function() {
+            return navigator.userAgent.match(/Opera Mini/i);
+        },
+        Windows: function() {
+            return navigator.userAgent.match(/IEMobile/i);
+        },
+        any: function() {
+            return isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows();
+        }
+    };
+    let addWindowScrollEvent = false;
+    setTimeout((() => {
+        if (addWindowScrollEvent) {
+            let windowScroll = new Event("windowScroll");
+            window.addEventListener("scroll", (function(e) {
+                document.dispatchEvent(windowScroll);
+            }));
+        }
+    }), 0);
+    "use strict";
+    if (isMobile.any()) {
+        document.body.classList.add("_touch");
+        let menuArrows = document.querySelectorAll(".menu__arrow");
+        if (menuArrows.length > 0) for (let index = 0; index < menuArrows.length; index++) {
+            const menuArrow = menuArrows[index];
+            menuArrow.addEventListener("click", (function(e) {
+                menuArrow.parentElement.classList.toggle("_active");
+            }));
+        }
+    } else document.body.classList.add("_pc");
+    const iconMenu = document.querySelector(".menu__icon");
+    const menuBody = document.querySelector(".menu__body");
+    if (iconMenu) iconMenu.addEventListener("click", (function(e) {
+        document.body.classList.toggle("_lock");
+        iconMenu.classList.toggle("_active");
+        menuBody.classList.toggle("_active");
+    }));
+    window["FLS"] = true;
+    isWebp();
+})();
